e6a33f413d7a2254982b3c1895a8cbef6a43d0d7
Strikerepic <cbrennan08@ladueschools.net>
2022-11-09 17:33:32 +0000

    Nothing New!

--- ConsumerLab_Code/Main.java
+++ ConsumerLab_Code/Main.java
@@ -7,7 +7,6 @@ public class Main {
         System.out.println(Review.starRating("/workspace/APComputerScienceProjects/ConsumerLab_Code/MacsReview.txt") + " Star Resturant");
         System.out.println(Review.fakeReview("/workspace/APComputerScienceProjects/ConsumerLab_Code/CustomReview.txt"));
         System.out.println(Review.fakeOnlyReallyGood("/workspace/APComputerScienceProjects/ConsumerLab_Code/CustomReview.txt"));
-
         System.out.println(Review.sentimentVal("abheek"));
 
    
