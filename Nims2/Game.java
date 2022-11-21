07637bd3d23fae82a1fc70ef582a2b3b5e5b07a7
Strikerepic <cbrennan08@ladueschools.net>
2022-11-21 21:30:37 +0000

    Heyo! The Code Has Been Updated!

---
+++ Nims2/Game.java
@@ -0,0 +1,97 @@
+package Nims2;
+
+import java.util.Scanner;
+public class Game {
+    private int pieces;
+    private Player p1;
+    private Player p2;
+    private Player currentPlayer;
+    private Scanner sc = new Scanner(System.in);
+
+    //Sets up the game with a random amount of pieces between 10 and 50
+    //Sets up the players so they can be accessed
+    public Game(Player p1, Player p2){
+        pieces = (int) (Math.random() * 41) + 10;
+        this.p1 = p1;
+        this.p2 = p2;
+    }
+
+    //Allows a player to take a specific amount of pieces.
+    //Adds the amount of pieces taken to the user's score.
+    public int takePiece(){
+
+        int take = 0;
+       //TO DO: Grab the user amount of pieces and repeat if it not allowed.
+
+        while (!isLegal(take)){
+            
+            System.out.println("There are "+ pieces+" remaining!");
+            System.out.println("How many pices do you want to take?");
+            take = sc.nextInt();
+        }
+        //TO DO: Adjust the pieces
+      
+        pieces = pieces - take;
+
+        System.out.println("There are "+ pieces+" remaining!");
+        System.out.println("-----------------------");
+        
+        return take;
+    }
+
+    
+    //**TO DO**//
+    public Player getNextPlayer(){
+    //Changes which players turn it is and returns the current player.
+      
+    if(currentPlayer == p2)
+     {
+         return currentPlayer = p1;
+       }
+        else {
+         return currentPlayer = p2;
+        }
+    }
+
+    //Checks whether or not the user's requested move is allowed or not.
+    public boolean isLegal(int x){
+
+        if(pieces >= 3 ) {
+
+             if(x <= pieces/2 && x > 0) {
+               return true;
+             }
+             else {
+                System.out.println("The amount that you take must be less than or equal to half the number of pieces that remain!");
+            return false;
+                }
+            }
+        else {
+
+            if((x == 1) && (x>0)) {
+            return true;
+            }
+            else {
+                return false;
+            }
+            }
+        }
+
+        
+        
+    
+
+
+
+    //DO NOT CHANGE
+    public boolean isComplete(){
+
+        if (pieces == 0){ return true;}
+        return false;
+    }
+
+    //DO NOT CHANGE
+    public void setFirstPlayer(Player p){
+        currentPlayer = p;
+    }
+}
---
+++ Nims2/Main.java
@@ -0,0 +1,75 @@
+package Nims2;
+
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+
+        //Asks the user to input thier name for the player
+        System.out.println("What is player1's name?: ");
+        String p1Name = sc.nextLine();
+        Player p1 = new Player(p1Name);
+        //TO DO: Create Player 1
+
+        //Asks the user to input their name for the player.
+        System.out.println("What is player2's name?: ");
+        String p2Name = sc.nextLine();
+        Player p2 = new Player(p2Name);
+        //TO DO: Create Player 
+
+        String again = "Literally any string";
+        Player currentPlayer;
+        
+        //Runs the game
+        while (!again.equals("q")){
+            
+            //Generates the game
+            //TO DO: Create "game"
+
+            Game game = new Game(p1, p2);
+
+            //TO DO: Set the a random player as the current player
+            double goofyNumber = Math.random();
+               
+            if(goofyNumber > 0.5) {
+                    currentPlayer = p1;
+                    game.setFirstPlayer(p1);
+                }
+                else {
+                    currentPlayer = p2;
+                    game.setFirstPlayer(p2);
+                }
+
+           
+            //This is the loop in which the game will be played
+
+            while(!game.isComplete()){
+               System.out.println("NEW");
+                System.out.println(currentPlayer.getName() + " it's your turn!");
+                
+
+
+                if(currentPlayer == p1) {
+                    p1.adjustScore(game.takePiece());
+                }
+                else {
+                    p2.adjustScore(game.takePiece());
+                }
+
+                currentPlayer = game.getNextPlayer();
+                
+                
+            }
+
+
+            System.out.println(currentPlayer.getName() + " Won!!");
+            System.out.println(p1.getName() +" had removed "+ p1.getScore()+" pieces!");
+            System.out.println(p2.getName() +" had removed "+ p2.getScore()+" pieces!");
+            System.out.println("------------------------------------------");
+            System.out.println("Enter q to quit, enter anything else to play again.");
+            again = sc.nextLine();
+        }
+        System.out.println("Thank you for playing!");
+    }
+}
+
