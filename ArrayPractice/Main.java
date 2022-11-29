f5ab1296b2879bd093a9f639e636f29e5f4279d7
Strikerepic <cbrennan08@ladueschools.net>
2022-11-29 18:54:26 +0000

    Arrays

--- ArrayPractice/Main.java
+++ ArrayPractice/Main.java
@@ -5,7 +5,7 @@ public class Main {
 
         int numList[] = {1,2,3,4,5,6,7,8,9,10,11};
 
-        for(int index = numList.length-1; index >= 0; index--) {
+        for(int index = numList.length-1; index >= 0; index--) {   // -1 needed beacuse array is always one less then we think it will be.
             System.out.println(numList[index]);
         }
     }
