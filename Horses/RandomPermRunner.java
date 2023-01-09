0bf01a712630321e5dafbfcb9b2b0a371e1f7819
Strikerepic <cbrennan08@ladueschools.net>
2023-01-09 13:38:08 +0000

    Home work for activity 3.7.4

---
+++ Horses/RandomPermRunner.java
@@ -0,0 +1,56 @@
+package Horses;
+import java.util.ArrayList;
+
+
+public class RandomPermRunner
+{
+  public static void main(String[] args)
+  {
+    int[] randNums = RandomPermutation.next(10);
+    
+    ArrayList<String> horsesArray = new ArrayList<String>();
+     
+    System.out.println(java.util.Arrays.toString(randNums));
+
+
+    
+
+    String horseNames[] = {"null", "Horse1","Horse2","Horse3","Horse4","Horse5","Horse6","Horse7","Horse8","Horse9","Horse10"};
+
+
+    for(int i = 0; i < 10; i++) {
+
+        int hold = randNums[i];
+
+        System.out.println(horseNames[hold]);
+
+
+        horsesArray.add(horseNames[hold]);
+
+
+
+
+    }
+
+
+    System.out.println(horsesArray);
+
+
+
+    for(int i = 0; i<10; i = i+2) {
+
+        System.out.println("Ladies and Gentlemen, " + horsesArray.get(i) + " and " + horsesArray.get(i +1) + "  are partners!!!" );
+
+    }
+
+
+
+
+        
+        
+
+        
+    
+
+  }
+}
